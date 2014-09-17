e0ca52908c63e421a1011199d49f119d85aad6e2
Tomasz <tomasz.debek@connectmedica.com>
2014-09-17 16:50:35 +0200

    Nazwa apki

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917141347) do
+ActiveRecord::Schema.define(version: 20140917143551) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20140917141347) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
